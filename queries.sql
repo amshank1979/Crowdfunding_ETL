f6f67be7bb94cad2e65dcc96455e5c2ff523f50c
Starla Halliday <mepseha@gmail.com>
2023-09-10 15:46:06 -0400

    Updated Queries

---
+++ queries.sql
@@ -0,0 +1,57 @@
+-- List the contact_id, last & first name, how much pledged coupled with category/subcategory and descriptions. 
+SELECT campaign.contact_id, 
+contacts.last_name, 
+contacts.first_name,
+campaign.pledged,
+campaign.category_id,
+category.category,
+campaign.subcategory_id,
+subcategory.subcategory
+FROM campaign
+JOIN contacts
+ON campaign.contact_id=contacts.contact_id
+JOIN category
+ON campaign.category_id = category.category_id
+JOIN subcategory
+ON campaign.subcategory_id = subcategory.subcategory_id
+order by pledged DESC;
+
+-- List the contact_id, last & first name, how much pledged. 
+SELECT campaign.contact_id, 
+contacts.last_name, 
+contacts.first_name,
+campaign.pledged
+FROM campaign
+JOIN contacts
+ON campaign.contact_id=contacts.contact_id
+order by pledged DESC;
+
+-- List count of category_id total instances. 
+SELECT campaign.category_id, category.category, COUNT(campaign.category_id) AS frequency_counts
+FROM campaign
+JOIN category
+ON campaign.category_id = category.category_id
+GROUP BY campaign.category_id, category.category
+ORDER BY frequency_counts DESC, campaign.category_id;
+
+-- List count of subcategory_id total instances. 
+SELECT campaign.subcategory_id, subcategory.subcategory, COUNT(campaign.subcategory_id) AS frequency_counts
+FROM campaign
+JOIN subcategory
+ON campaign.subcategory_id = subcategory.subcategory_id
+GROUP BY campaign.subcategory_id, subcategory.subcategory
+ORDER BY frequency_counts DESC, campaign.subcategory_id;
+
+-- List total count of outcomes. 
+SELECT outcome, COUNT(category_id) AS frequency_counts
+FROM campaign
+GROUP BY outcome
+ORDER BY frequency_counts DESC, outcome;
+
+-- List total count of outcomes by category. 
+SELECT campaign.outcome, category.category, COUNT(campaign.category_id) AS frequency_counts
+FROM campaign
+JOIN category
+ON campaign.category_id = category.category_id
+GROUP BY category.category, campaign.outcome
+ORDER BY frequency_counts DESC, outcome;
